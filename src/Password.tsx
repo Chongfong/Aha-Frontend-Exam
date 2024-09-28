f5749fea4ae94a4661b30a0496ed436be35dcf50
churin <wl00796865@gmail.com>
2024-09-28 12:30:28 +0800

    feat(core): add debounce

--- src/Password.tsx
+++ src/Password.tsx
@@ -13,6 +13,8 @@ interface ValidationStatus {
 function Password() {
   const [showErrors, setShowErrors] = useState(false);
   const [password, setPassword] = useState('');
+  const [debouncedPassword, setDebouncedPassword] = useState(password);
+
   const checkValidation = (inputPassword: string): ValidationStatus => ({
     length: inputPassword.length >= 8,
     uppercase: /[A-Z]/.test(inputPassword),
@@ -21,13 +23,26 @@ function Password() {
     specialChar: /[!@#$%^&*(),.?":{}|<>]/.test(inputPassword),
   });
 
+  const debounce = (func: Function, delay: number) => {
+    let timeoutId: ReturnType<typeof setTimeout>;
+    return (...args: any[]) => {
+      if (timeoutId) clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => func(...args), delay);
+    };
+  };
+
+  const debouncedHandleChange = debounce((newPassword: string) => {
+    setDebouncedPassword(newPassword);
+  }, 300);
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newPassword = event.target.value;
     setPassword(newPassword);
     setShowErrors(true);
+    debouncedHandleChange(newPassword);
   };
 
-  const validationStatus = checkValidation(password);
+  const validationStatus = checkValidation(debouncedPassword);
 
   const validationCriteria = [
     { key: 'uppercase', text: 'Have at least one uppercase letter' },
